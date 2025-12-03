06d7a626993d4a902d6044011b5ad222e950185b
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-12-03 12:05:29 +0100

    feat: add basic navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,20 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
   return(
-    <>
-      NavBar
-    </>
+    <Navbar expand='lg' className="bg-body-tertiary">
+      <Container>
+        <NavLink to="/" className="navbar-brand">Weather Dashboard</NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav className="me-auto">
+            <NavLink to="/about" className="nav-link">About</NavLink>
+            <NavLink to="/profile" className="nav-link">Profile</NavLink> 
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
